9b1219fc607f42b80bbb4fca7c982795fd4cb49a
Christian Sauka <christian@sauka.de>
2019-09-28 18:47:46 +0200

    Groundwork in file process_data.py

--- data/process_data.py
+++ data/process_data.py
@@ -10,7 +10,7 @@ def clean_data(df):
 
 
 def save_data(df, database_filename):
-    pass  
+    pass
 
 
 def main():
@@ -24,12 +24,12 @@ def main():
 
         print('Cleaning data...')
         df = clean_data(df)
-        
+
         print('Saving data...\n    DATABASE: {}'.format(database_filepath))
         save_data(df, database_filepath)
-        
+
         print('Cleaned data saved to database!')
-    
+
     else:
         print('Please provide the filepaths of the messages and categories '\
               'datasets as the first and second argument respectively, as '\
@@ -40,4 +40,5 @@ def main():
 
 
 if __name__ == '__main__':
+    print("Program started")
     main()
