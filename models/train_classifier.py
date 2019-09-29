9fa3011980651fbd3e3e8d5cd0a482453eaa26bd
Christian Sauka <christian@sauka.de>
2019-09-29 11:19:29 +0200

    Made groundwork in train_classfier.py

--- models/train_classifier.py
+++ models/train_classifier.py
@@ -1,24 +1,56 @@
 import sys
+import re
+import pandas as pd
+from sqlalchemy import create_engine
+
+from nltk.tokenize import word_tokenize
+
+from sklearn.model_selection import train_test_split
+from sklearn.pipeline import Pipeline
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.multioutput import MultiOutputClassifier
+from sklearn.feature_extraction.text import CountVectorizer, TfidfTransformer
+from sklearn.metrics import classification_report, multilabel_confusion_matrix
+from sklearn.grid_search import GridSearchCV
 
 
 def load_data(database_filepath):
-    pass
+    """load messages from database"""
+    engine = create_engine("sqlite:///" + database_filepath)
+    df = pd.read_sql("messages", engine).iloc[:1000]
+    X = df["message"]
+    Y = df.iloc[:, -36:]
 
 
 def tokenize(text):
-    pass
+    """tokenizer to be used within the CountVectorizer"""
+    text = re.sub(r"[^a-zA-Z0-9]", " ", text).lower()
+    return word_tokenize(text)
 
 
 def build_model():
-    pass
+    pipeline = Pipeline([("vectorizer", CountVectorizer(tokenizer=tokenize)),
+                     ("tfidf", TfidfTransformer()),
+                     ("clf", MultiOutputClassifier(DecisionTreeClassifier()))])
+    parameters = {"clf__estimator__max_depth": [5, 10, None],
+                  "clf__estimator__min_samples_split": [2, 3, 4]}
+    model = GridSearchCV(pipeline, param_grid=parameters)
+
+    return model
 
 
 def evaluate_model(model, X_test, Y_test, category_names):
-    pass
+    """make and score predictions based on the given model"""
+    Y_pred = model.predict(X_test)
+    for col in range(Y_pred.shape[1]):
+        print(category_names[col].upper())
+        print(classification_report(Y_test.values[:,col], Y_pred[:,col]))
 
 
 def save_model(model, model_filepath):
-    pass
+    """save the trained model in a pickle file"""
+    pickle.dump(model, open(model_filepath, 'wb'))
 
 
 def main():
@@ -27,13 +59,13 @@ def main():
         print('Loading data...\n    DATABASE: {}'.format(database_filepath))
         X, Y, category_names = load_data(database_filepath)
         X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=0.2)
-        
+
         print('Building model...')
         model = build_model()
-        
+
         print('Training model...')
         model.fit(X_train, Y_train)
-        
+
         print('Evaluating model...')
         evaluate_model(model, X_test, Y_test, category_names)
 
